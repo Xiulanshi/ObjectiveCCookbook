69cfa39255e1d283c6872f2cfe52a05b3d04d8b7
Xiulan Shi <xiulanshi2013@gmail.com>
2016-03-30 11:16:15 -0400

    Updated

---
+++ FirstiOSApp/FirstiOSApp/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  FirstiOSApp
+//
+//  Created by Xiulan Shi on 3/30/16.
+//  Copyright © 2016 Xiulan Shi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ FirstiOSApp/FirstiOSApp/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  FirstiOSApp
+//
+//  Created by Xiulan Shi on 3/30/16.
+//  Copyright © 2016 Xiulan Shi. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
